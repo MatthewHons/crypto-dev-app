1e320b1865d949e8f65cacb3cc7e4850bd78b8fa
Matthew Hons <devwithmatt@gmail.com>
2022-06-17 16:03:32 +0200

    Adding the search bar for crypto

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,17 +4,34 @@ import {Link} from 'react-router-dom';
 import {Card, Row, Col, Input} from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import { InsertRowBelowOutlined } from '@ant-design/icons';
 
-const Cryptocurrencies = () => {
-    const { data: cryptoList, isFetching} = useGetCryptosQuery();
-    const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
-    
+const Cryptocurrencies = ({simplified}) => {
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching} = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+        setCryptos(filteredData);
+
+    }, [cryptosList, searchTerm])
+
+    if(isFetching) return 'Loading...'; 
+
+
     return (
         <>
-            <Row gutters={[32,32]} className="crypto-card-container">
-                {cryptos.map((currency) => (
+        {!simplified && (
+            <div className='search-crypto'>
+            <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
+        </div>
+        )}
+            <Row gutter={[32,32]} className="crypto-card-container">
+                {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id} >
                         <Link to={`/crypto/${currency.id}`}>
                             <Card
